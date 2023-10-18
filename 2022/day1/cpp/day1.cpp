d482cccfd8fdb10a1d856bc109eeba1b73cc03a6
Luke Roepe <ldroepe@gmail.com>
2023-10-17 22:59:36 -0400

    Use a heap
    The previous solution relied on std::partial_sort to get the max n
    elements. The disadvantage to this is that I am using linear storage,
    and the partial_sort operation is therefore logarithmic in complexity.
    
    If a (max) heap is used instead, then for N = 3, constant complexity and
    storage is achieved. A quirk of asking for the max 3 values is used in
    the implementation: there is only ever 4 elements in the (max) heap at
    any one time. This means that selecting the minimum element, moving it
    to the end of the vector, and then calling std::vector::pop_back still
    preserves the heap property. This is not true for all values of N.

--- 2022/day1/cpp/day1.cpp
+++ 2022/day1/cpp/day1.cpp
@@ -37,6 +37,36 @@ int find_most_calories()
 
 #else
 
+constexpr int n = 3;
+
+template <typename Container, typename Comp = std::less<typename Container::value_type> >
+class heap_back_inserter
+{
+private:
+    Container& c;
+    Comp comp;
+
+    using this_ref_t = heap_back_inserter<Container, Comp>&;
+
+public:
+    explicit heap_back_inserter(Container& container, Comp compare=Comp()):
+        c(container), comp(compare) {}
+    this_ref_t operator++() { return *this; }
+    this_ref_t operator*() { return *this; }
+    this_ref_t operator=(typename Container::value_type v) {
+        c.push_back(std::move(v));
+        std::push_heap(std::begin(c), std::end(c), comp);
+        return *this;
+    }
+
+};
+
+template <typename Container, typename Comp = std::less<typename Container::value_type> >
+heap_back_inserter<Container, Comp> make_heap_inserter(Container& c, Comp comp = Comp())
+{
+    return heap_back_inserter<Container, Comp>(c, comp);
+}
+
 int count_calories(std::istream& in)
 {
     int total = 0;
@@ -46,14 +76,35 @@ int count_calories(std::istream& in)
     return total;
 }
 
-template <typename OutIt>
-OutIt collect_all_calorie_counts(std::istream& in, OutIt out)
+// push_back --> O(1) b/c we reserved
+// std::push_heap --> O(lg(N+1)), N=3
+// std::min_element --> O(N-1), N=3
+// pop_back --> O(1)
+std::vector<int> collect_all_calorie_counts(std::istream& in)
 {
+    std::vector<int> calories;
+    calories.reserve(n+1); // only storing 4 elements at most
+    auto heap_inserter = make_heap_inserter(calories);
+
+    for(int i = 0; i < n; ++i)
+        *heap_inserter = count_calories(in); // requires in has at least n lines
 
     while(in)
-        *out++ = count_calories(in);
-    
-    return out;
+    {
+        const int c = count_calories(in);
+        *heap_inserter = c;
+
+        // remove the smallest element from a max heap
+        // relise on the fact that there are 3 elements in the heap, so removing
+        // the smallest element in this way will still preserve the heap
+        // property
+        // can't use c[begin|end] here bc we need a non-const ref for std::swap
+        const auto smallest_elem = std::min_element(std::begin(calories), std::end(calories));
+        std::swap(*smallest_elem, calories.back());
+        calories.pop_back();
+    }
+
+    return calories;
 }
 
 #endif
@@ -71,16 +122,16 @@ int main()
 
 #else
 
-    constexpr int n = 3;
-    std::vector<int> calorie_counts;
-    collect_all_calorie_counts(inFile, std::back_inserter(calorie_counts));
+    const std::vector<int> calorie_counts = collect_all_calorie_counts(inFile);
 
+    /*
     // Complexity is (first-last)log(middle-first), and for small n, this means
     // we basically have linear time
     std::partial_sort(std::begin(calorie_counts), 
                       std::begin(calorie_counts) + n + 1, 
                       std::end(calorie_counts),
                       std::greater<>());
+    */
 
     const int max_n_calories = 
         std::accumulate(std::cbegin(calorie_counts),
